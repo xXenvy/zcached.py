46aaf8b9089840ad429943611b94b935667f5a1b
xXenvy <quentisxd@gmail.com>
2024-03-17 22:25:34 +0100

    feat: Add deserializer to `__all__`

--- zcached/__init__.py
+++ zcached/__init__.py
@@ -5,6 +5,7 @@ from .connection import Connection
 from .backoff import ExponentialBackoff
 from .result import Result
 from .serializer import Serializer, SupportedTypes
+from .deserializer import Deserializer
 from .reader import Reader
 
 __all__: Final[Tuple[str, ...]] = (
@@ -14,6 +15,7 @@ __all__: Final[Tuple[str, ...]] = (
     "Result",
     "Serializer",
     "SupportedTypes",
+    "Deserializer",
     "Reader",
 )
 

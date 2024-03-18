c0a61dd0db954d3540c3114ea95c4894182ece16
xXenvy <quentisxd@gmail.com>
2024-03-18 22:56:48 +0100

    refactor(deserializer): Use `read_until` method in deserializers

--- zcached/deserializer.py
+++ zcached/deserializer.py
@@ -20,57 +20,56 @@ class Deserializer:
         reader:
             The Reader object containing the payload data.
         """
-        handlers: dict[str, Callable[[Reader], Any]] = {
-            "+": self.deserialize_sstr,
-            "$": self.deserialize_str,
-            ":": self.deserialize_int,
-            ",": self.deserialize_float,
-            "#": self.deserialize_bool,
-            "_": self.deserialize_none,
-            "*": self.deserialize_array,
-            "%": self.deserialize_map,
+        handlers: dict[bytes, Callable[[Reader], Any]] = {
+            b"+": self.deserialize_sstr,
+            b"$": self.deserialize_str,
+            b":": self.deserialize_int,
+            b",": self.deserialize_float,
+            b"#": self.deserialize_bool,
+            b"_": self.deserialize_none,
+            b"*": self.deserialize_array,
+            b"%": self.deserialize_map,
         }
-        current_first_char: str = chr(reader.current[0])
-        return handlers[current_first_char](reader)
+        return handlers[reader.read(1)](reader)
 
     @staticmethod
     def deserialize_str(reader: Reader) -> str:
         """Method to deserialize a payload data to string."""
-        reader.read()
-        return reader.read().decode()[1::]
+        reader.read_until(b"\n")
+        return reader.read_until(b"\n").decode()
 
     @staticmethod
     def deserialize_sstr(reader: Reader) -> str:
         """Method to deserialize a payload data to string."""
-        return reader.read().decode()[1::]
+        return reader.read_until(b"\n").decode()
 
     @staticmethod
     def deserialize_int(reader: Reader) -> int:
         """Method to deserialize a payload data to integer."""
-        return int(reader.read()[1::])
+        return int(reader.read_until(b"\n"))
 
     @staticmethod
     def deserialize_float(reader: Reader) -> float:
         """Method to deserialize a payload data to float."""
-        return float(reader.read()[1::])
+        return float(reader.read_until(b"\n"))
 
     @staticmethod
     def deserialize_bool(reader: Reader) -> bool:
         """Method to deserialize a payload data to boolean."""
-        return reader.read() == b"#t"
+        return reader.read_until(b"\n") == b"t"
 
     @staticmethod
     def deserialize_none(reader: Reader) -> None:
         """Method to deserialize a payload data to None."""
-        reader.read()
+        reader.read_until(b"\n")
         return None
 
     def deserialize_array(self, reader: Reader) -> List[Any]:
         """Method to deserialize a payload data to array."""
-        array_size: int = int(reader.read()[1::])
+        array_size: int = int(reader.read_until(b"\n"))
         return [self.process(reader) for _ in range(array_size)]
 
     def deserialize_map(self, reader: Reader) -> dict[str, Any]:
         """Method to deserialize a payload data to dictionary."""
-        map_size: int = int(reader.read()[1::])
+        map_size: int = int(reader.read_until(b"\n"))
         return {self.process(reader): self.process(reader) for _ in range(map_size)}

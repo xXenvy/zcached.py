817a148dd6606c0ffbc1750fcb01210a549eac0e
xXenvy <xpimpek01@gmail.com>
2024-04-16 00:05:20 +0200

    feat: Add example

---
+++ examples/handling_errors.py
@@ -0,0 +1,19 @@
+from typing import Union
+from zcached import ZCached, Result, Errors
+
+client = ZCached(host="localhost", port=1234)
+
+
+def handle_error(error_message: str, key: Union[str, None] = None) -> None:
+    if error_message == Errors.ConnectionClosed:
+        raise ConnectionError("Connection closed.")
+    if error_message == Errors.ConnectionClosed:
+        return  # The connection was dropped, but managed to restore it.
+    if error_message == Errors.not_found(key):
+        raise RuntimeError("Key not found.")
+
+
+result: Result[str] = client.get("key123")
+if result.error:
+    handle_error(error_message=result.error, key="key123")
+
